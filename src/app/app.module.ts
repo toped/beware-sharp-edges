94b24dc72e8cdba3c8ab2e54f87a380a30816efa
Tope Daramola <tope.daram@gmail.com>
2018-03-16 13:18:13 -0500

    [R1803 - First Steps] : Animations 😐

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -18,7 +19,8 @@ import { AboutComponent } from './about/about.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+    styleUrls: ['./home.component.css'],
+    animations: [
+        trigger('goals', [
+            transition('* => *', [
+                query(':enter', style({ opacity: 0}), {optional: true}),
+                query(':enter', stagger('300ms', [
+                    animate('.6s ease-in', keyframes([
+                        style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+                        style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+                        style({opacity: 1, transform: 'translateY(0)', offset: 1}),
+                    ]))
+                ]), {optional: true}),
+                query(':leave', stagger('300ms', [
+                    animate('.6s ease-in', keyframes([
+                        style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+                        style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+                        style({opacity: 0, transform: 'translateY(-75%)', offset: 1}),
+                    ]))
+                ]), {optional: true})
+            ])
+        ])
+    ]
 })
+
 export class HomeComponent implements OnInit {
 
     itemCount:number;
     btnText:string = 'Add an item';
     goalText:string = 'My first life goal';
-    goals = [];
+    goals = ['test1', 'test2', 'test3'];
 
     constructor() { }
 
@@ -27,6 +50,14 @@ export class HomeComponent implements OnInit {
     addItem() {
         this.goals.push(this.goalText);
         this.goalText = '';
+        this.itemCount = this.goals.length;
+    }
+
+    /**
+    Remove items from bucket list
+    */
+    removeItem(i) {
+        this.goals.splice(i, 1);
         this.itemCount = this.goals.length;
     }
 
